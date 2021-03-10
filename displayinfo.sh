00d90fb382dcf09072796dfd393c051a0d167650
ryandariz <49538441+ryandariz@users.noreply.github.com>
2021-03-09 22:03:10 -0600

    Update displayinfo.sh

--- displayinfo.sh
+++ displayinfo.sh
@@ -4,3 +4,12 @@ echo "Hello, $LOGNAME"
 echo "Current date is `date`"
 echo "User is `whoami`"
 echo "Current directory `pwd`"
+
+
+echo "Hello $Logname \n" > greetingmsg
+
+echo "Current date is `date` \n" > greetingmsg
+
+echo "User is `whoami` \n" > greetingmsg
+
+echo "Current directory `pwd` \n" > greetingmsg
